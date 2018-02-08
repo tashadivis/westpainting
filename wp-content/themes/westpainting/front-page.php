c248794f450ba866a4555f8e64a0f27ff068aa57
Tasha Divis <tashadivis@gmail.com>
2018-02-08 02:01:40 -0800

    Placeholder Hero Image

--- wp-content/themes/westpainting/front-page.php
+++ wp-content/themes/westpainting/front-page.php
@@ -16,8 +16,9 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-
+			<?php include("template-parts/hero.php"); ?>
 			<?php include("template-parts/estimate-cta.php"); ?>
+			<?php include("template-parts/services.php"); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
---
+++ wp-content/themes/westpainting/template-parts/hero.php
@@ -0,0 +1,8 @@
+<div class="hero">
+	<img src="<?php echo esc_url( home_url( '/wp-content/uploads/2018/02/ladder-guy.jpg' ) ); ?>">
+	<div class="hero-content">
+		<h1 class="yellowtext">Lorem Ipsum Dolor Sit Amet</h1>
+		<p class="whitetext">Lorem Ipsum is simple dummy text in the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text.</p>
+		<a class="yellowbtn ctabtn" href="#">Read More</a>
+	</div>
+</div>
