276f3e38477caf4f96eff55134c2010ac4c1c183
Tasha Divis <tashadivis@gmail.com>
2018-02-08 00:13:41 -0800

    Responsive Header & Menu

--- wp-content/themes/westpainting/front-page.php
+++ wp-content/themes/westpainting/front-page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all pages
+ * The template for displaying FRONT PAGE
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
@@ -17,13 +17,7 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-			<?php
-			while ( have_posts() ) : the_post();
-
-				get_template_part( 'template-parts/content', 'page' );
-
-			endwhile; // End of the loop.
-			?>
+			
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
--- wp-content/themes/westpainting/template-parts/content-page.php
+++ wp-content/themes/westpainting/template-parts/content-page.php
@@ -12,6 +12,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<hr>
 	</header><!-- .entry-header -->
 
 	<?php westpainting_post_thumbnail(); ?>
