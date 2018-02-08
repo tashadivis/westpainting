3d7008d77ed6aed9a7411e71c57da49d8db56470
Tasha Divis <tashadivis@gmail.com>
2018-02-08 02:11:50 -0800

    Services Placeholder

---
+++ wp-content/themes/westpainting/template-parts/services.php
@@ -0,0 +1,28 @@
+<div id="services">
+	<div class="entry-header">
+		<h1>Our Services</h1>
+		<hr>
+	</div>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+	<div class="service">
+		<img src="<?php echo esc_url( home_url( '/wp-content/uploads/2018/02/house.jpg' ) ); ?>">
+		<div class="description">
+			<h3>Painting</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod tempor</p>
+		</div>
+	</div>
+	<div class="service">
+		<img src="<?php echo esc_url( home_url( '/wp-content/uploads/2018/02/house.jpg' ) ); ?>">
+		<div class="description">
+			<h3>Painting</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod tempor</p>
+		</div>
+	</div>
+	<div class="service">
+		<img src="<?php echo esc_url( home_url( '/wp-content/uploads/2018/02/house.jpg' ) ); ?>">
+		<div class="description">
+			<h3>Painting</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod tempor</p>
+		</div>
+	</div>
+</div>
