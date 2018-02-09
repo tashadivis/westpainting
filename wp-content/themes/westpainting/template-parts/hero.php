011248ffd8db9144458bd643a616c204f0e80d00
Tasha Divis <tashadivis@gmail.com>
2018-02-09 13:13:23 -0800

    Hero Image Responsive

--- wp-content/themes/westpainting/template-parts/hero.php
+++ wp-content/themes/westpainting/template-parts/hero.php
@@ -1,5 +1,5 @@
 <div class="hero">
-	<img src="<?php echo esc_url( home_url( '/wp-content/uploads/2018/02/ladder-guy.jpg' ) ); ?>">
+	<!-- <img src="<?php //echo esc_url( home_url( '/wp-content/uploads/2018/02/paintbro.jpg' ) ); ?>"> -->
 	<div class="hero-content">
 		<h1 class="yellowtext">Lorem Ipsum Dolor Sit Amet</h1>
 		<p class="whitetext">Lorem Ipsum is simple dummy text in the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text.</p>
