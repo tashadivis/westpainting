2c9eb3967a2c71701062c3b6dd7d4d9ae14e5d64
Tasha Divis <tashadivis@gmail.com>
2018-02-09 15:51:21 -0800

    Services Page

---
+++ wp-content/themes/westpainting/page-our-services.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying FRONT PAGE
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WestPainting
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<?php include("template-parts/services.php"); ?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
--- wp-content/themes/westpainting/template-parts/services.php
+++ wp-content/themes/westpainting/template-parts/services.php
@@ -1,4 +1,4 @@
-<!-- if is home page, only show "featured" posts -->
+<?php if(is_page('home')) { ?>
 
 <div id="services">
 	<div class="entry-header">
@@ -6,25 +6,84 @@
 		<hr>
 	</div>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-	<div class="service">
-		<img src="<?php echo esc_url( home_url( '/wp-content/uploads/2018/02/house.jpg' ) ); ?>">
-		<div class="description">
-			<h3>Painting</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod tempor</p>
+	<div id="servicesContainer">
+		<div class="service">
+			<img src="<?php echo esc_url( home_url( '/wp-content/uploads/2018/02/house.jpg' ) ); ?>">
+			<div class="description">
+				<h3>Painting</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod tempor</p>
+			</div>
+		</div>
+		<div class="service">
+			<img src="<?php echo esc_url( home_url( '/wp-content/uploads/2018/02/house.jpg' ) ); ?>">
+			<div class="description">
+				<h3>Painting</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod tempor</p>
+			</div>
+		</div>
+		<div class="service">
+			<img src="<?php echo esc_url( home_url( '/wp-content/uploads/2018/02/house.jpg' ) ); ?>">
+			<div class="description">
+				<h3>Painting</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod tempor</p>
+			</div>
 		</div>
 	</div>
-	<div class="service">
-		<img src="<?php echo esc_url( home_url( '/wp-content/uploads/2018/02/house.jpg' ) ); ?>">
-		<div class="description">
-			<h3>Painting</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod tempor</p>
-		</div>
+	<a class="ctabtn bluebtn" href="<?php echo esc_url( home_url( '/our-services' ) ); ?>">View all Services</a>
+</div>
+
+<?php } elseif(is_page('our-services')) { ?>
+
+<div id="services" class="our-services">
+	<div class="entry-header">
+		<h1>Our Services</h1>
+		<hr>
 	</div>
-	<div class="service">
-		<img src="<?php echo esc_url( home_url( '/wp-content/uploads/2018/02/house.jpg' ) ); ?>">
-		<div class="description">
-			<h3>Painting</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod tempor</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+	<div id="servicesContainer">
+		<div class="service">
+			<img src="<?php echo esc_url( home_url( '/wp-content/uploads/2018/02/house.jpg' ) ); ?>">
+			<div class="description">
+				<h3>Painting</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod tempor</p>
+			</div>
+		</div>
+		<div class="service">
+			<img src="<?php echo esc_url( home_url( '/wp-content/uploads/2018/02/house.jpg' ) ); ?>">
+			<div class="description">
+				<h3>Painting</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod tempor</p>
+			</div>
+		</div>
+		<div class="service">
+			<img src="<?php echo esc_url( home_url( '/wp-content/uploads/2018/02/house.jpg' ) ); ?>">
+			<div class="description">
+				<h3>Painting</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod tempor</p>
+			</div>
+		</div>
+		<div class="service">
+			<img src="<?php echo esc_url( home_url( '/wp-content/uploads/2018/02/house.jpg' ) ); ?>">
+			<div class="description">
+				<h3>Painting</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod tempor</p>
+			</div>
+		</div>
+		<div class="service">
+			<img src="<?php echo esc_url( home_url( '/wp-content/uploads/2018/02/house.jpg' ) ); ?>">
+			<div class="description">
+				<h3>Painting</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod tempor</p>
+			</div>
+		</div>
+		<div class="service">
+			<img src="<?php echo esc_url( home_url( '/wp-content/uploads/2018/02/house.jpg' ) ); ?>">
+			<div class="description">
+				<h3>Painting</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod tempor</p>
+			</div>
 		</div>
 	</div>
 </div>
+
+<?php } ?>
