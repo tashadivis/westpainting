5e36fa1610c019a1fd2117c61f122548dbc96435
Tasha Divis <tashadivis@gmail.com>
2018-02-09 20:22:27 -0800

    Testimonials Slider & Cleanup

--- wp-content/themes/westpainting/template-parts/testimonials.php
+++ wp-content/themes/westpainting/template-parts/testimonials.php
@@ -3,7 +3,19 @@
 		<span class="greytext">Testimonials</span>
 	</h1>
 	<div class="slider testimonials">
-		<div></div>
+		<div>
+			<img src="<?php echo esc_url( home_url( '/wp-content/themes/westpainting/inc/quote-left.svg' ) ); ?>">
+			<p>We have used Alek's company 'West Painting & Decorating' for the last 10 years. Alek was recommended by my contractor and we have been extremely happy with the many painting/staining jobs he has done for us at different houses. He does a wonderful job at a very fair price. We highly recommend him and his crew for any of your painting/staining needs.</p>
+			<!-- <img src="<?php //echo esc_url( home_url( '/wp-content/themes/westpainting/inc/quote-right.svg' ) ); ?>"> -->
+			<p class="author">Margaret Yaworski & Paul Lukaitis</p>
+		</div>
+		<div>
+			<img src="<?php echo esc_url( home_url( '/wp-content/themes/westpainting/inc/quote-left.svg' ) ); ?>">
+			<p>Aleksander Jagla has worked for me for approximately 28 years, as both a maintenance man and a painting contractor. He has proven to be dependable and provide excellent service and results.</p>
+			<!-- <img src="<?php //echo esc_url( home_url( '/wp-content/themes/westpainting/inc/quote-right.svg' ) ); ?>"> -->
+			<p class="author">Ed Striker</p>
+			<p class="company">Admin - Burnaby Center</p>
+		</div>
 	</div>
 </div>
 
